8985b7e83aa3ed195f880148ba9bf9891316b618
Akul Biswas <biswasakul287@gmail,com>
2025-07-02 01:56:26 +0600

    removed photo required

--- src/Component/Register.jsx
+++ src/Component/Register.jsx
@@ -170,7 +170,7 @@ const Register = () => {
                     id="photoURL"
                     name="photoURL"
                     type="url"
-                    required
+                    
                     className="w-full px-4 py-3 pl-10 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 placeholder-gray-500 dark:placeholder-gray-400"
                     placeholder="Enter your photo URL"
                   />
